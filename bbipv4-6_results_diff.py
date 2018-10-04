57697384080c4d2ac0ab557ed5fbdddf9f3a2a1d
Bob Dooling <bob@doolings.org>
2018-10-04 21:09:42 +0000

    updated bbipv4-6_results_diff python script

--- bbipv4-6_results_diff.py
+++ bbipv4-6_results_diff.py
@@ -1,8 +1,8 @@
 from libnmap.parser import NmapParser
 import xmltodict
 
-nmapv4_report = NmapParser.parse_fromfile('/tmp/v4-6test/ip6-tcp-10.xml')
-v6xml = open("/tmp/v4-6test/hostnames-ip6-tcp-10.xml","r")
+nmapv4_report = NmapParser.parse_fromfile('/tmp/v4-6test/dualhomed-ip4-tcp-26k.xml')
+v6xml = open("/tmp/v4-6test/dualhomed-ip6-tcp-26k.xml","r")
 v6results = xmltodict.parse(v6xml.read())
 
 for eachv4host in nmapv4_report.hosts:
@@ -13,13 +13,14 @@ for eachv4host in nmapv4_report.hosts:
         hostv4name = "".join(hostnamelist[0].strip("."))
     else:
         hostv4name = "".join(hostnamelist).strip(".")
-    print(hostv4name)
+    # print(hostv4name)
     v4portset = set()
     for eachv4port in hostv4ports:
         # print(eachv4port)
         v4portset.add(eachv4port[0])
     for eachv6host in v6results['nmaprun']['host']:
         match = False
+        v6portset = set()
         hostv6nameornames = eachv6host['hostnames']['hostname']
         if type(hostv6nameornames) == list:
             # print("We have a list!: ",hostv6name)
@@ -29,8 +30,7 @@ for eachv4host in nmapv4_report.hosts:
         # print(hostv6name)
         if hostv4name == hostv6name:
             match = True
-            print("We have a match!")
-            v6portset = set()
+            # print("We have a match!: ",hostv4name,hostv6name)
             # print(hostv4name,hostv6name)
             hostv6ports = eachv6host['ports']
             for eachv6port in hostv6ports['port']:
@@ -39,7 +39,7 @@ for eachv4host in nmapv4_report.hosts:
                     # print(eachv6port['@portid'])
                     v6portset.add(int(eachv6port['@portid']))
                 except TypeError:
-                    print("Only one port open on this system...!!!!!!! ")
+                    # print("Only one port open on this system...!!!!!!! ")
                     v6portlist = []
                     v6portlist = [(hostv6ports['port'])]
                     for eachv6port in v6portlist:
@@ -54,13 +54,13 @@ for eachv4host in nmapv4_report.hosts:
             # print("Hostname does not match; returning to 'for eachv6host' loop...")
             continue
     if match != True:
-        print("Hmmm.... hostnames don't match for this one... ????")
+        print("Hmmm.... no matching hostname in IP6 output for",hostv4name,"...")
     # print("The ports accessible via IPv4 are: ",v4portset)
     # print("The ports accessible via IPv6 are: ",v6portset)
     v4diffset = v4portset.difference(v6portset)
-    if len(v4diffset) != 0:
-        print("The ports accessible only via IPv4 are: ",v4diffset)
+    # if len(v4diffset) != 0:
+        # print("The ports accessible only via IPv4 are: ",v4diffset)
     v6diffset = v6portset.difference(v4portset)
     if len(v6diffset) != 0:
-        print("The ports accessible only via IPv6 are: ",v6diffset)
+        print("Ports accessible only via IPv6 for",hostv6name,"are:",v6diffset)
 
